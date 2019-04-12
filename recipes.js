e8ca2c3d9c289198b8704267edfbf60c8edc7717
Tormod Flesjø <tormod.flesjo@gmail.com>
2019-04-12 11:48:02 +0200

    instructor code

--- recipes.js
+++ recipes.js
@@ -1,40 +1,85 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Recipe = require('./Recipe.js');
 const data = require('./data.js');
 
-const RecipeSchema = new Schema({
-    title: { type: String, unique: true},
-    level: { type: String,  enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] },
-    ingredientes: { type: Array},
-    cuisine: {type: String, required:true},
-    dishType: {type: String, enum: ['Breakfast', 'Dish','Snack', 'Drink','Dessert', 'Other' ]},
-    image:{type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg}'},
-    duration: {type: Number, min:[0]},
-    creator: {type: String},
-    created: {type: Date, default: Date.now},
-
+/* Schema is in Recipe.js */
+/* Connecting to Mongodb with the name recipeApp */
+ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+  .then(() => {	 
+    console.log('Connected to Mongo!');	    
   })
+  .catch(err => {
+    console.error('Error connecting to mongo', err);	    
+  });	  
 
+  /* Creating a new recipe based on the schema from Recipe.js */
+ Recipe.create({
+  title: "Garden Stuffed Baked Potatoes",
+  level: "Amateur Chef",
+  ingredients: [
+    "4 large potatoes",
+    "2 tablespoons butter",
+    "1 small onion, chopped",
+    "1 (10 ounce) package chopped frozen broccoli, thawed",
+    "1/2 cup ranch-style salad dressing",
+    "1 tablespoon vegetable oil",
+    "2 teaspoons dried parsley",
+    "salt and pepper to taste",
+  ],
+  cuisine: "American",
+  dishType: "Breakfast",
+  image: "hhttps://images.media-allrecipes.com/userphotos/560x315/450020.jpg",
+  duration: 60,
+  creator: "Thor" 
+  })
+  .then(receta => {
+    console.log('Nueva receta:', receta.title);
+  })
+  .catch(err => {
+    console.log('Error al crear nueva receta', err);
+  });
+  
+  /* Creating many */
+ Recipe.insertMany(data)
+  .then(recetas => {
+  console.log('Recetas importadas desde data.js');
+  recetas.map(receta => receta.title).forEach(title => {
+      console.log('Receta importada:', title);
+    })
+  })
+  .catch(err => {
+    console.log('Error al importar recetas de data.js', err);
+  }); 
 
-  const Recipe = mongoose.model('Recipe', RecipeSchema);
-
-  mongoose.connect('mongodb://localhost/recipeApp')
+  /* Updates one */
+ Recipe.updateOne(
+  { title: "Chocolate Chip Cookies" },
+  { $set: { duration: 45 }}
+  )
   .then(() => {
-    console.log('Connected to Mongo!');
-    Recipe.collection.drop()
-
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+    console.log('Chocolate Chip Cookies ha sido modificada');
+  })
+  .catch(err => {
+    console.log('Error, Chocolate Chip Cookies', err);
   });
 
-Recipe.create({
- title:'Spaghetti',
- level: 'Easy Peasy',
- cuisine: 'Italian' 
+  /* Deletes one */
+ Recipe.deleteOne(
+  {title: "Asian Glazed Chicken Thighs"}
+  )
+  .then(() => {
+    console.log('Asian Glazed Chicken Thighs ha sido eliminada');
+  })
+  .catch(err => {
+    console.log('Error, Asian Glazed Chicken Thighs', err);
+  })
+  /* CLOESES THE CONNECTION */
+  .then(()=>{
+ mongoose.connection.close()
+  .then(() => {
+    console.log('Disconnected to Mongo!');
+  })
+  .catch(err => {
+    console.error('Error disconnecting to mongo', err);
+  }); 
 })
-
-.then(()=> Recipe.insertMany(data))
-.then(()=> Recipe.updateOne({ title: "Rigatoni alla Genovese"},{ duration: 100})
-.then(()=> Recipe.deleteOne({title:"Carrot Cake"}))
-.then(()=>mongoose.disconnect())
-.catch(err=>console.log('You have an error, time to debug',err)))
